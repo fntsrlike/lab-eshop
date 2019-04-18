f7a17cac91d456d4bcffa3da9500688081a1843a
Roushi Lin <fntsrlike@gmail.com>
2019-04-18 17:39:19 +0800

    feat(auth): add Facebook linking url on shop page

--- app/models/o_auth_provider.rb
+++ app/models/o_auth_provider.rb
@@ -8,4 +8,8 @@ class OAuthProvider < ApplicationRecord
   def self.from_omniauth(auth)
     find_or_create_by(uid: auth.uid, provider: auth.provider)
   end
+
+  def self.facebook
+    where(provider: 'facebook').first
+  end
 end
