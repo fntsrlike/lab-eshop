7d17deb9cd6a2fbd9458ba5015b1e7738dba95f3
Ruoshi Lin <fntsrlike@gmail.com>
2019-04-23 16:07:34 +0800

    feat(shop): add subscribe link on shop page

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,7 +4,8 @@ class ShopsController < ApplicationController
   def index
     provider = current_user.providers.facebook
     pages = FacebookUser.new(provider).pages
+    subscriptions = provider.subscriptions.map { |s| s.oid }
 
-    render locals: {provider: provider, pages: pages}
+    render locals: {provider: provider, pages: pages, subscriptions: subscriptions}
   end
 end
