95b0f415b5460dd08db1f3b82976e41d2b7fc33a
Ruoshi Lin <fntsrlike@gmail.com>
2019-04-23 11:12:48 +0800

    refactor(shop): extract getting page process to a method

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -3,13 +3,17 @@ class ShopsController < ApplicationController
 
   def index
     provider = current_user.providers.facebook
+    pages = get_pages(provider)
 
-    pages = []
+    render locals: {provider: provider, pages: pages}
+  end
+
+  private
+  def get_pages(provider)
     if !provider.expired? && provider.facebook?
       graph = Koala::Facebook::API.new(provider.token)
       pages = graph.get_connections("me", "accounts")
     end
-
-    render locals: {provider: provider, pages: pages}
+    pages ||= []
   end
 end
