6db95b68ea5ef635f3bc483eede3d3ef6c689c96
Ruoshi Lin <fntsrlike@gmail.com>
2019-04-24 16:43:29 +0800

    fix(shop): error on shop page without FB linking

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,9 +2,14 @@ class ShopsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    pages = []
+    subscriptions = []
+
     provider = current_user.providers.facebook
-    pages = FacebookUser.new(provider).pages
-    subscriptions = provider.subscriptions.map { |s| s.oid }
+    if provider
+      pages = FacebookUser.new(provider).pages
+      subscriptions = provider.subscriptions.map { |s| s.oid }
+    end
 
     render locals: {provider: provider, pages: pages, subscriptions: subscriptions}
   end
