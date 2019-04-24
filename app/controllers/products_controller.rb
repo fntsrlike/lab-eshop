152b9a35dda7b70a05c341b044493e55c3431bfb
Ruoshi Lin <fntsrlike@gmail.com>
2019-04-24 17:14:21 +0800

    feat(product): add edit feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action(:set_resource, only: [:new, :edit])
+  before_action(:set_resource, only: [:new, :edit, :update])
 
   def index
     render locals: {products: current_user.shop.products}
@@ -16,6 +16,14 @@ class ProductsController < ApplicationController
       render(:new)
     end
   end
+
+  def update
+    if @resource.update(products_params)
+      redirect_to(products_path, notice: 'Resource was successfully updated.')
+    else
+      render(:edit)
+    end
+end
 
   private
   def products_params
