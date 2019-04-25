c8b419fd023098396ef2c1ddc01193e87a59efdb
Ruoshi Lin <fntsrlike@gmail.com>
2019-04-25 10:19:32 +0800

    feat(db): add recipient & delivered time on orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_015543) do
+ActiveRecord::Schema.define(version: 2019_04_25_021008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,9 @@ ActiveRecord::Schema.define(version: 2019_04_25_015543) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "buyer_id"
+    t.string "recipient"
+    t.string "address"
+    t.datetime "delivered_at"
     t.index ["buyer_id"], name: "index_orders_on_buyer_id"
   end
 
