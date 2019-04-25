ec84adf3eedaecc4a5da76ef32ac85d806285265
Ruoshi Lin <fntsrlike@gmail.com>
2019-04-25 10:53:10 +0800

    feat(db): add default amount on ordered_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_021008) do
+ActiveRecord::Schema.define(version: 2019_04_25_022928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2019_04_25_021008) do
   create_table "ordered_items", force: :cascade do |t|
     t.bigint "product_id"
     t.bigint "order_id"
-    t.integer "amount"
+    t.integer "amount", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_ordered_items_on_order_id"
